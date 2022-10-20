bd5d67b09a6938c126fbb269d04efa193d1e458e
DurbanD <andrew@durbantech.com>
2022-10-20 12:06:08 -0400

    Added authentication to Get /user/ so only an admin account can get a list of all users in the db

--- app.py
+++ app.py
@@ -125,6 +125,16 @@ def add_user():
 # Get Users
 @app.route('/user', methods=['GET'])
 def get_users():
+    # Require Authorization header
+    try:
+        access_token = request.headers['Authorization']
+    except:
+        return Response(status=401)
+    session = Session.query.filter_by(access_token=access_token).first()
+    # Return 401 if accesss token does not belong to an admin account
+    if session.user.admin == False:
+        return Response(status=401)
+
     users = User.query.all()
     result = users_schema.dump(users)
     return jsonify(result)
@@ -290,7 +300,7 @@ def check_auth():
 
     return jsonify({"auth":loginStatus, "exp":expiration or None})
 
-def createAdminAccount():
+def createDefaultAdminAccount():
     email = 'admin@example.com'
     password = 'dbAdmin'
     username = 'admin'
